08cee169efe819797725581139f741f0cabe6848
Phil Denhoff <phil@denhoff.ca>
2023-12-14 10:54:49 -0800

    non-pessimized get all notes

--- apps/proxy/models/notes.ts
+++ apps/proxy/models/notes.ts
@@ -1,41 +1,26 @@
 import { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 
 type UserID = string;
+type Note = {
+  id: number;
+  content: string;
+};
 
 export const userIdFromLogin = (login: string): UserID => {
   return `user::${login}`;
 };
 
-const prepareQueryUserUnprocessedNotes = (db: DB) => {
-  return db.prepareQuery<
-    [string, number],
-    { content: string; id: number },
-    { user_id: string }
-  >(
-    "SELECT id, content FROM notes WHERE (user_id = :user_id) AND (times_processed < 1) ORDER BY id LIMIT 300 OFFSET 0;"
+export const allUserUnprocessedNotes = (userId: UserID, db: DB): Note[] => {
+  const rows = db.query<[number, string]>(
+    "UPDATE notes SET times_read = times_read + 1, times_processed = times_processed + 1 WHERE (user_id = :user_id) AND (times_processed < 1) RETURNING id, content;",
+    { user_id: userId }
   );
-};
-
-const markNotesRead = (notes: { id: number }[], db: DB) => {
-  const updateQuery = db.prepareQuery<
-    [number],
-    { times_processed: number },
-    { id: number }
-  >(
-    "UPDATE notes SET times_read = times_read + 1, times_processed = 1 WHERE id = :id"
-  );
-  notes.forEach((row) => {
-    updateQuery.allEntries({ id: row.id });
+  return rows.map((row) => {
+    return {
+      id: row[0],
+      content: row[1],
+    };
   });
-  updateQuery.finalize();
-};
-
-export const allUserUnprocessedNotes = (userId: UserID, db: DB) => {
-  const query = prepareQueryUserUnprocessedNotes(db);
-  const rows = query.allEntries({ user_id: userId });
-  query.finalize();
-  markNotesRead(rows, db);
-  return rows;
 };
 
 export const addNote = (userId: UserID, content: string, db: DB) => {
