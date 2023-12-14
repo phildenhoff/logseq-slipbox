5d49ee4449dbdb63cca6973362be123a9ec3fc44
Phil Denhoff <phil@denhoff.ca>
2023-12-14 13:01:42 -0800

    feat: users can close plugin screen

--- apps/plugin/src/App.svelte
+++ apps/plugin/src/App.svelte
@@ -1,31 +1,36 @@
 <script lang="ts">
   import svelteLogo from "./assets/svelte.svg";
+  import ClickOutCaptureContainer from "./components/ClickOutCaptureContainer.svelte";
   import Counter from "./components/Counter.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/logo.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<ClickOutCaptureContainer>
+  <main>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/logo.svg" class="logo" alt="Vite Logo" />
+      </a>
+      <a href="https://svelte.dev" target="_blank">
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </a>
+    </div>
+    <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <div class="card">
+      <Counter />
+    </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
+    <p>
+      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
+        >SvelteKit</a
+      >, the official Svelte app framework powered by Vite!
+    </p>
 
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
+    <p class="read-the-docs">
+      Click on the Vite and Svelte logos to learn more
+    </p>
+  </main>
+</ClickOutCaptureContainer>
 
 <style>
   .logo {
---
+++ apps/plugin/src/components/ClickOutCaptureContainer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  const close = () => {
+    logseq.hideMainUI();
+  };
+</script>
+
+<div
+  id="clickOutCaptureContainer"
+  class="backdrop-blur-md"
+  on:click={close}
+  on:keydown={(event) => (event.key === "Escape" ? close() : () => {})}
+>
+  <slot />
+</div>
+
+<style>
+  #clickOutCaptureContainer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
