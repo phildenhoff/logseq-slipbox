ece604dcb4ed3737095826a2422e5826a590b3c4
Phil Denhoff <phil@denhoff.ca>
2023-12-14 12:28:06 -0800

    add button to display app

--- apps/plugin/src/main.ts
+++ apps/plugin/src/main.ts
@@ -1,8 +1,26 @@
-import './app.css'
-import App from './App.svelte'
+import "./app.pcss";
+import "./app.pcss";
+import "@logseq/libs";
+import App from "./App.svelte";
 
-const app = new App({
-  target: document.getElementById('app')
-})
+const main = async () => {
+  new App({
+    target: document.getElementById("app"),
+  });
 
-export default app
+  const createModel = () => ({
+    show: () => {
+      console.log("showing");
+      logseq.showMainUI();
+    },
+  });
+
+  logseq.provideModel(createModel());
+
+  logseq.App.registerUIItem("toolbar", {
+    key: "slipbox",
+    template: `<a data-on-click="show" id="slipbox" class="toolbar-button" >⌘</a>`,
+  });
+};
+
+logseq.ready(main).catch(console.error);
