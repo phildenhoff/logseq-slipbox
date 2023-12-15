b4427f07002835a39fc3dc14bc143ec26e58ef32
Phil Denhoff <phil@denhoff.ca>
2023-12-15 15:52:18 -0800

    build: add scripts to build & run

---
+++ apps/proxy/run_container.sh
@@ -0,0 +1,10 @@
+# create a volume for app state, like the database & tailscale config
+# no-op if it already exists
+docker volume create slipbox
+
+docker run \
+  --platform linux/amd64\
+  --name=slipbox \
+  -v slipbox:/data \
+  -p 8000:8000 \
+  slipbox
